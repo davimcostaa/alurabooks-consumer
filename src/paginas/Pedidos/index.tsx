48182916caff5404c8d614fca678e4bd75c05098
Davi Costa <davim.costa@outlook.com>
2023-08-31 18:20:10 -0300

    Estilização dos pedidos

---
+++ src/paginas/Pedidos/index.tsx
@@ -0,0 +1,50 @@
+import { AbBotao } from 'ds-alurabooks'
+import React from 'react'
+import './Pedidos.css'
+
+const Pedidos = () => {
+  return (
+    <section className='pedidos'>
+        <h1>Meus pedidos</h1>
+        <div className='pedido'>
+            <ul>
+                <li>Pedido: <strong>12345</strong></li>
+                <li>Data do pedido: <strong>12/09/2022</strong></li>
+                <li>Valor total: <strong>R$ 42</strong></li>
+                <li>Entrega realizada em: <strong>13/09/2022</strong></li>
+            </ul>
+            <AbBotao texto='Detalhes' />
+        </div>
+        <div className='pedido'>
+            <ul>
+                <li>Pedido: <strong>12345</strong></li>
+                <li>Data do pedido: <strong>12/09/2022</strong></li>
+                <li>Valor total: <strong>R$ 42</strong></li>
+                <li>Entrega realizada em: <strong>13/09/2022</strong></li>
+            </ul>
+            <AbBotao texto='Detalhes' />
+        </div>
+        <div className='pedido'>
+            <ul>
+                <li>Pedido: <strong>12345</strong></li>
+                <li>Data do pedido: <strong>12/09/2022</strong></li>
+                <li>Valor total: <strong>R$ 42</strong></li>
+                <li>Entrega realizada em: <strong>13/09/2022</strong></li>
+            </ul>
+            <AbBotao texto='Detalhes' />
+        </div>
+        <div className='pedido'>
+            <ul>
+                <li>Pedido: <strong>12345</strong></li>
+                <li>Data do pedido: <strong>12/09/2022</strong></li>
+                <li>Valor total: <strong>R$ 42</strong></li>
+                <li>Entrega realizada em: <strong>13/09/2022</strong></li>
+            </ul>
+            <AbBotao texto='Detalhes' />
+        </div>
+        
+    </section>
+  )
+}
+
+export default Pedidos
